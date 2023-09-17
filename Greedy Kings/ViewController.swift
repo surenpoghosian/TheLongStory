542534849d4b42a3d2e8f04c6e44935b9b62263d
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-09-17 13:47:11 +0400

    Initial Commit

---
+++ Greedy Kings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greedy Kings
+//
+//  Created by Suren Poghosyan on 17.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

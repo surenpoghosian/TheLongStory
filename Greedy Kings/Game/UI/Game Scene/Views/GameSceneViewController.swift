2dd51515a2794fa71208591536bf371fdf5cca0a
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-09-22 16:50:46 +0400

    architecture-LS-20 solved by adding file to every folder

---
+++ Greedy Kings/Game/UI/Game Scene/Views/GameSceneViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GameSceneViewController.swift
+//  Greedy Kings
+//
+//  Created by Suren Poghosyan on 22.09.23.
+//
+
+import Foundation
---
+++ Greedy Kings/Game/UI/Settings/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Greedy Kings
+//
+//  Created by Suren Poghosyan on 22.09.23.
+//
+
+import Foundation

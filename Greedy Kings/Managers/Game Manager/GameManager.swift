0497ac99ab5a67df79455b2c67e859a3ce6f5bfb
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-09-27 14:49:45 +0400

    gamelogic-LS-9 GameManager, UserManager, LevelManager initiated

--- Greedy Kings/Managers/Game Manager/GameManager.swift
+++ Greedy Kings/Managers/Game Manager/GameManager.swift
@@ -1,8 +1,13 @@
 //
-//  LevelBuilder.swift
+//  GameManager.swift
 //  Greedy Kings
 //
 //  Created by Suren Poghosyan on 27.09.23.
 //
 
 import Foundation
+
+
+final class GameManager {
+    
+}

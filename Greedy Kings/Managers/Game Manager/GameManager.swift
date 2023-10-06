c3ea516ee6345ee92dbde530b987f6a4274e6270
Garik <gar.hovsepian@gmail.com>
2023-10-06 21:06:02 +0400

    gamelogic-LS-24 GameManager initiated with mimnum functionality, alse added HealthManager to it

--- Greedy Kings/Managers/Game Manager/GameManager.swift
+++ Greedy Kings/Managers/Game Manager/GameManager.swift
@@ -7,7 +7,81 @@
 
 import Foundation
 
-
 final class GameManager {
+    private var currentPlayer: Player!
+    private var battleResult: BattleResult!
     
+    init(currentPlayer: Player!, battleResult: BattleResult!) {
+        initializeBattleResult()
+    }
+    
+    func initializeBattleResult() {
+        battleResult = BattleResult(player1Shots: 0,
+                                    player1Hits: 0,
+                                    player2Shots: 0,
+                                    player2Hits: 0)
+    }
+    
+    func startGame() {
+        currentPlayer = .player1
+    }
+    
+    func switchPlayerTurn() {
+        if currentPlayer == .player1 {
+            currentPlayer = .player2
+        } else {
+            currentPlayer = .player1
+        }
+    }
+    
+    func updateHits() {
+        if currentPlayer == .player1 {
+            battleResult.player1Hits += 1
+            checkIsGameFinished()
+            
+        } else {
+            battleResult.player2Hits += 1
+        }
+        checkIsGameFinished()
+    }
+    
+    func updateShots() {
+        if currentPlayer == .player1 {
+            battleResult.player1Shots += 1
+            
+        } else {
+            battleResult.player2Shots += 1
+        }
+    }
+    
+    func checkIsGameFinished() {
+        if HealthManager.shared.player1health <= 0 || HealthManager.shared.player2health <= 0 {
+            setWinner(winner: currentPlayer)
+            gameFinished()
+            
+        } else {
+            updateHealth(currentPlayer: currentPlayer)
+            switchPlayerTurn()
+        }
+    }
+    
+    func updateHealth(currentPlayer: Player) {
+        if currentPlayer == .player1 {
+            HealthManager.shared.player2health -= 20
+        } else {
+            HealthManager.shared.player2health -= 20
+        }
+    }
+    
+    func setWinner(winner: Player) {
+        battleResult.winner = winner
+    }
+    
+    func onGameInterrupted() {
+        
+    }
+    
+    func gameFinished() {
+        
+    }
 }
---
+++ Greedy Kings/Managers/Game Manager/HealthManager/HealthManager.swift
@@ -0,0 +1,17 @@
+//
+//  HealthManager.swift
+//  Greedy Kings
+//
+//  Created by Garik Hovsepian on 06.10.23.
+//
+
+import Foundation
+
+final class HealthManager {
+
+    static let shared = HealthManager()
+    var player1health = 100.0
+    var player2health = 100.0
+    
+    private init(){}
+}
